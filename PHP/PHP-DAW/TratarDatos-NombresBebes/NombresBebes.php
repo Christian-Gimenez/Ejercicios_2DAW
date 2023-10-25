fe22b5cdf7b84c02706e4212de601855138e449c
Christian-Gimenez <christian.gimenez@educa.madrid.org>
2023-10-25 11:53:36 +0200

    Terminados deberes JS. Empezado el hacerlos en PHP modificados:     JavaScript/TratarDatos-NombresBebes/NombresBebes.html nuevos archivos: PHP/PHP-DAW/TratarDatos-NombresBebes/NombresBebes.html nuevos archivos: PHP/PHP-DAW/TratarDatos-NombresBebes/NombresBebes.php nuevos archivos: PHP/PHP-DAW/TratarDatos-NombresBebes/nomnac21-01.php nuevos archivos: PHP/PHP-DAW/TratarDatos-NombresBebes/nomnac21.csv

---
+++ PHP/PHP-DAW/TratarDatos-NombresBebes/NombresBebes.php
@@ -0,0 +1,12 @@
+<?php
+include_once "nomnac21-01.php";
+
+//Spliteamos en un array los datos del CSV
+$datos = preg_split(";", $datos);
+//Mapeamos para limpiar los espacios en blanco de cada dato
+$datos = array_map(fn($dato) => trim($dato), $datos); //En array_map(1ºCallback, 2ºArray)
+//Filtramos para eliminar los registros vacíos del array
+$datos = array_filter($datos, fn($reg) => $reg !== ""); //En array_filter(1ºArray, 2ºCallback)
+//Dividimos en niños y niñas. ¡REVISAR!
+$niñosYniñas = preg_split(";;", implode("", $datos));
+?>

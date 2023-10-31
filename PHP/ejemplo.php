f98627ae08364eb91eb89150a9caa6d8aba5ed2f
Christian-Gimenez <christian.gimenez@educa.madrid.org>
2023-10-31 20:49:33 +0100

    Terminada clase 31-10-23

---
+++ PHP/ejemplo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <p>Hola mundo html desde php</p>
+  <?php
+  echo "<script>alert('hola mundo');</script>";
+  echo "<h1>Hola mundo desde php</h1>"
+  ?>
+</body>
+
+</html>
---
+++ Python/hello_world.py
@@ -0,0 +1,8 @@
+print("hello world")
+
+fruits = ["apple", "banana", "cherry"]
+
+if "banana" in fruits:
+  print("yes")
+else:
+  print("no")

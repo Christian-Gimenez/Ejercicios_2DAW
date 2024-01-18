4d5e982164018367daab98f9e4d955d25c0200be
Christian-Gimenez <christian.gimenez@educa.madrid.org>
2024-01-18 11:14:36 +0100

    Terminada funcion GenerarHtml JS

--- JavaScript/U05P01/funciones_constructoras/app-fun.js
+++ JavaScript/U05P01/funciones_constructoras/app-fun.js
@@ -4,12 +4,12 @@
 const Utilidades = {};
 
 //Genera número aleatorio entre uno y otro (ambos inclusive)
-Utilidades.numeroAleatorio = function(desde, hasta) {
+Utilidades.numeroAleatorio = function (desde, hasta) {
   return Math.floor(Math.random() * (hasta - desde + 1) + desde);
 }
 
 //Genera una fecha 1 mes en el futuro + 1 día más
-Utilidades.fechaUnMesEnElFuturo = function() {
+Utilidades.fechaUnMesEnElFuturo = function () {
   const fecha = new Date();
   fecha.setMonth(fecha.getMonth() + 1);
   fecha.setDate(fecha.getDate() + 1);
@@ -19,9 +19,9 @@ Utilidades.fechaUnMesEnElFuturo = function() {
 //id:string, sinDev:number, conDev:number, normal:number
 function Tren(id, sinDev, conDev, normal) {
   /*Si sinDeb + conDev es + 50%, devolvemos (para que sea undefined el obj Tren) */
-  if(sinDev + conDev > (Tren.NUM_ASIENTOS * Tren.MAX_PORCENTAJE_DEVOLUCION)) return;
+  if (sinDev + conDev > (Tren.NUM_ASIENTOS * Tren.MAX_PORCENTAJE_DEVOLUCION)) return;
   /*Si todos los args tienen valor, ahora si construimos el Tren */
-  if(id && sinDev && conDev && normal) {
+  if (id && sinDev && conDev && normal) {
     this.id = id; //string
     this.asientosTotales = Tren.NUM_ASIENTOS; //number
     this.asientosOcupados = { //obj
@@ -39,12 +39,12 @@ Tren.MAX_PORCENTAJE_DEVOLUCION = 0.5; //number
 
 //Métodos estáticos Tren
 //Genera un id:string partiendo de un dia:Date y una horaSalida:string
-Tren.generarID = function(dia, horaSalida) {
+Tren.generarID = function (dia, horaSalida) {
   return dia.getDate() + horaSalida.replace(":", "");
 }
 
 //Genera un Tren partiendo de un dia:Date y una horaSalida:string
-Tren.generarTren = function(dia, horaSalida) {
+Tren.generarTren = function (dia, horaSalida) {
   //Establece el límite de la oferta (la mitad del total de NUM_ASIENTOS)
   const LIMITE_OFERTA = Tren.NUM_ASIENTOS * Tren.MAX_PORCENTAJE_DEVOLUCION;
 
@@ -84,19 +84,19 @@ Semana.DOMINGO = 0;
 //Semana.UN_DIA = 24 * 60 * 60 * 1000; Milisegundos
 
 //Métodos publicos de instancia
-Semana.prototype.rellenarHorarios = function(fechaInicio, fechaFinal){
+Semana.prototype.rellenarHorarios = function (fechaInicio, fechaFinal) {
   //Creamos un array de Date y lo rellenamos, desde fechaInicio hasta fechaFinal
   const dias = [];
-  for(let i = fechaInicio; i < fechaFinal; fechaInicio.setDate(fechaInicio.getDate() + 1)) {
+  for (let i = fechaInicio; i < fechaFinal; fechaInicio.setDate(fechaInicio.getDate() + 1)) {
     dias.push(new Date(fechaInicio.getTime()));
   }
 
   //Rellenamos la prop []Horario de Semana con un array de objs Horario
   this.horarios = dias.map(dia => { //Iteramos el array de dias Date
     //Creamos array vacío Horario en cada iteración, que será el horario de un sólo día
-    const horarioDia = []; 
+    const horarioDia = [];
     //Sacamos el día de la semana en cada iteración para luego rellenar dependiendo de si es sábado, domingo u otro
-    const diaSemana = dia.getDay(); 
+    const diaSemana = dia.getDay();
 
     //Rellenamos el array de Horario con las salidas normales ['09:40', '10:40', '12:20', '13:10', '15:10', '16:40', '17:40', '18:40', '19:30', '21:40'];
     horarioDia = Horario.SALIDAS.map(salida => {
@@ -104,12 +104,12 @@ Semana.prototype.rellenarHorarios = function(fechaInicio, fechaFinal){
     });
 
     //Pero si es sábado, agregamos 08:40 al principio
-    if(diaSemana == Semana.SABADO) {
+    if (diaSemana == Semana.SABADO) {
       horarioDia.unshift(Horario.generarHorario(dia, '08:40'));
     }
 
     //Y si es domingo, agregamos 20:10 como penúltimo
-    if(diaSemana == Semana.DOMINGO) {
+    if (diaSemana == Semana.DOMINGO) {
       horarioDia.splice((Horario.SALIDAS.length - 2), 0, Horario.generarHorario(dia, '20:10'));
     }
     return horarioDia;
@@ -123,7 +123,7 @@ function Horario(tren, hora) {
 }
 
 //Genera un obj tipo Horario partiendo de un dia:Date y salida:string
-Horario.generarHorario = function(dia, salida) {
+Horario.generarHorario = function (dia, salida) {
   //Generamos un Tren basandonos en el dia:Date y salida:string
   const tren = Tren.generarTren(dia, salida);
   //Generamos un horario introduciendole el Tren anterior y la salida:string
@@ -133,4 +133,90 @@ Horario.generarHorario = function(dia, salida) {
 
 Horario.SALIDAS = ['09:40', '10:40', '12:20', '13:10', '15:10', '16:40', '17:40', '18:40', '19:30', '21:40'];
 
+/*Impresión de elementos HTML */
+/**div
+ *    div
+ *      h2 DIAS
+ *      button * 10 (genTabla)
+ *    table
+ *      caption
+ *      thead
+ *        tr
+ *          td "Texto fijo" + "nºDía" (ej: "Trenes del día 13")
+ *      tbody
+ *        tr
+ *          td 
+ *        tr
+ *          td
+ *      tfoot
+ *        tr
+ *          td "Texto fijo" + "nºTrenes" (ej: "Número de trenes: 10")
+ */
+function GenerarHTML() { }
 
+GenerarHTML.crearNodo = function (tipo, txt = false) {
+  const nodo = document.createElement(tipo);
+  if (txt) {
+    nodoTxt = document.createTextNode(txt);
+    nodo.appendChild(nodoTxt);
+  }
+  return nodo;
+}
+
+GenerarHTML.crearBotones = function (titulo, arrayBotones) {
+  const div = GenerarHTML.crearNodo("div");
+  const h2 = GenerarHTML.crearNodo("h2", titulo);
+  div.appendChild(h2);
+  arrayBotones.forEach(dato => {
+    const button = GenerarHTML.crearNodo("button", dato);
+    div.appendChild(button);
+  });
+  return div;
+}
+
+GenerarHTML.crearTabla = function (titulo, arrCabecera, arrObjDatos, pie) {
+  const table = GenerarHTML.crearNodo("table");
+  const caption = GenerarHTML.crearNodo("caption", titulo);
+  const thead = crearTHead(arrCabecera);
+  const tbody = crearTBody(arrObjDatos);
+  const tfoot = crearTFoot(pie, arrCabecera.length);
+  table.appendChild(caption);
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  table.appendChild(tfoot);
+  return table;
+
+  function crearTHead(arr1D) {
+    const thead = GenerarHTML.crearNodo("thead");
+    const tr = GenerarHTML.crearNodo("tr");
+    arr1D.forEach(dato => {
+      const th = GenerarHTML.crearNodo("th", dato);
+      tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+    return thead;
+  }
+
+  function crearTBody(arr2D) {
+    const tbody = GenerarHTML.crearNodo("tbody");
+    arr2D.forEach(obj => {
+      const tr = GenerarHTML.crearNodo("tr");
+      for (let key in obj) {
+        const td = GenerarHTML.crearNodo("td", obj[key]);
+        tr.appendChild(td);
+      }
+      tbody.appendChild(tr);
+    });
+    return tbody;
+  }
+
+  function crearTFoot(txt, columnas) {
+    const tfoot = GenerarHTML.crearNodo("tfoot");
+    const tr = GenerarHTML.crearNodo("tr");
+    const td = GenerarHTML.crearNodo("td", txt);
+    td.setAttribute("colspan", columnas);
+    tr.appendChild(td);
+    tfoot.appendChild(tr);
+    return tfoot;
+  }
+}

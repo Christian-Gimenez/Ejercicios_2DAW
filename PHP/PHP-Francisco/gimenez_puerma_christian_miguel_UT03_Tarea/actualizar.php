e6301f5f32a7b0d3fc6f6e38bb13ddbf67b715a5
Christian-Gimenez <christian.gimenez@educa.madrid.org>
2024-02-23 13:37:22 +0100

    Empezando la docu

--- PHP/PHP-Francisco/gimenez_puerma_christian_miguel_UT03_Tarea/actualizar.php
+++ PHP/PHP-Francisco/gimenez_puerma_christian_miguel_UT03_Tarea/actualizar.php
@@ -1,5 +1,5 @@
 <?php
-//Ralizar la consulta de actualización de datos y la redirección a listado.php.
+//Realizar la consulta de actualización de datos y la redirección a listado.php.
 require_once "./db_conector.php";
 require_once "./db_utils.php";
 ?>
@@ -9,19 +9,23 @@ require_once "./db_utils.php";
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
   <title>Ejercicio: Actualizar/Cancelar modificación producto</title>
-  <link href="dwes.css" rel="stylesheet" type="text/css">
+  <link href="estilos./dwes.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
   <?php
+  //Si le dió al botón de "Actualizar"
   if (isset($_POST["actualizar"])) {
+    //Almacenamos todos los datos del formulario limpiando los datos
     $cod = trim($_POST["cod"]) ?? "";
     $nombre = trim($_POST["nombre"]) ?? "";
     $nombreCorto = trim($_POST["nombre_corto"]) ?? "";
     $descripcion = trim($_POST["descripcion"]) ?? "";
     $pvp = trim($_POST["PVP"]) ?? "";
+    //Y actualizamos la base de datos con un UPDATE
     actualizarProducto($db, $cod, $nombre, $nombreCorto, $descripcion, $pvp);
   } else if (isset($_POST["cancelar"])) {
+    //Si le dió al botón de "Cancelar"
   ?>
     ?>
     <div id="encabezado">
